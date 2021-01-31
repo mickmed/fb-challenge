8ce63bc67888d6d39358bcd4df8ae4d6c34a9a9e
mickmed <mick2090@gmail.com>
2021-01-31 07:13:00 -0500

    simple item calculation

--- src/Components/OrderReview/OrderReview.jsx
+++ src/Components/OrderReview/OrderReview.jsx
@@ -4,20 +4,36 @@ import "./OrderReview.css";
 const OrderReview = (props) => {
   useEffect(() => {});
 
+  const [quantity, setQuantity] = useState([]);
+  const [total, setTotal] = useState([]);
+
+  const counter = (e, id, price) => {
+    setQuantity((prev) => {
+      prev[id] = parseInt(e.target.value);
+      return [...prev];
+    });
+    setTotal((prev) => {
+      prev[id] = parseInt(e.target.value * price)
+      return [...prev]
+    })
+  };
+
   return (
     <div className="order-review">
       <h3>Order Review</h3>
-      <div className='order'>
+      <div className="order">
         <h6>Please review your order</h6>
 
         <div className="header">
           <h3 className="name">item</h3>
           <h3 className="price">price</h3>
           <h3 className="qty">quantity</h3>
-          <h3 className='discount'>discount</h3>
+          <h3 className="discount">discount</h3>
+          <h3 className="total">total</h3>
+
         </div>
         <section className="items">
-          {props.order.map((item) => (
+          {props.order.map((item, index) => (
             <div className="item">
               {/* <img src={item.imgUrl} /> */}
 
@@ -28,10 +44,18 @@ const OrderReview = (props) => {
                 <h4>{item.price}</h4>
               </div>
               <div className="qty">
-                <input type="text" value="1" />
+                <input
+                  type="text"
+                  name={`quantity ${index}`}
+                  value={quantity[index] || 0}
+                  onChange={(e) => counter(e, index, item.price)}
+                />
               </div>
               <div className="discount">
                 <h4>%</h4>
+              </div>
+              <div className="total">
+                <input type='text' name='total' value={quantity[index] * item.price || 0}/>
               </div>
             </div>
           ))}
--- src/Data/data.js
+++ src/Data/data.js
@@ -1,34 +1,29 @@
 export const Order = [
   {
     name: "Long-Sleeve Shaker Cardigan Sweater(A)",
-    price: "5.98",
+    price: 60,
     imgUrl:
       "https://cdn-fsly.yottaa.net/5ba91e43312e581893f90c9d/www.fullbeauty.com/v~4b.324/dw/image/v2/BBKT_PRD/on/demandware.static/-/Sites-masterCatalog_WomanWithin/default/dw5d92cf16/images/hi-res/0056_13682_mc_1147.jpg?sw=240&sh=346&sm=fit&yocs=1_6_",
   },
   {
     name: "Embellished Drape Tunic(B)",
-    price: "12.98",
+    price: 70,
     imgUrl:
       "https://cdn-fsly.yottaa.net/5ba91e43312e581893f90c9d/www.fullbeauty.com/v~4b.324/dw/image/v2/BBKT_PRD/on/demandware.static/-/Sites-masterCatalog_Roamans/default/dweb3a7c8b/images/hi-res/0537_16778_mc_8350.jpg?sw=240&sh=346&sm=fit&yocs=1_6_",
   },
   {
     name: "Embroided Knit Tunic(C)",
-    price: "11.98",
+    price: 10,
     imgUrl:
       "https://cdn-fsly.yottaa.net/5ba91e43312e581893f90c9d/www.fullbeauty.com/v~4b.324/dw/image/v2/BBKT_PRD/on/demandware.static/-/Sites-masterCatalog_WomanWithin/default/dwcbbd4cc9/images/on-hover/0007_19498_ma_0001.jpg?sw=240&sh=346&sm=fit&yocs=1_6_",
   },
   {
     name: "Camp Rose Pleat-Neck Tank(D)",
-    price: "21.98",
+    price: 20,
     imgUrl:
       "https://cdn-fsly.yottaa.net/5ba91e43312e581893f90c9d/www.fullbeauty.com/v~4b.324/dw/image/v2/BBKT_PRD/on/demandware.static/-/Sites-masterCatalog_Catherines/default/dw81c2f755/images/on-hover/2642_26343_ma_0001.jpg?sw=240&sh=346&sm=fit&yocs=1_6_",
   },
-  {
-    name: "Perect Printed Short-Sleeve Boat-Neck(E)",
-    price: "5.98",
-    imgUrl:
-      "https://cdn-fsly.yottaa.net/5ba91e43312e581893f90c9d/www.fullbeauty.com/v~4b.324/dw/image/v2/BBKT_PRD/on/demandware.static/-/Sites-masterCatalog_WomanWithin/default/dw0784fce1/images/on-hover/0037_34386_ma_0001.jpg?sw=240&sh=346&sm=fit&yocs=1_6_",
-  },
+ 
 ];
 
 export const User = [
