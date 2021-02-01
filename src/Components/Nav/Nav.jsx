c6399d4935a80207f7a34d7f8ea7f0f94feac0f5
mickmed <mick2090@gmail.com>
2021-02-01 03:21:38 -0500

    adjust route

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -7,14 +7,14 @@ const Nav = (props) => {
   const active = { fontWeight: "bold", color: "red" };
   return (
     <nav style={yAxis > headerHeight ? headerStyle : {}}>
-      <NavLink exact to={"/" || "/shipping"} activeStyle={active}>
+      <NavLink exact to={"/shipping"} activeStyle={active}>
         Shipping
       </NavLink>
 
       <NavLink to="/billing" activeStyle={active}>
         Billing
       </NavLink>
-      <NavLink to="/order-review" activeStyle={active}>
+      <NavLink exact to={"/" || "/order-review"} activeStyle={active}>
         Order
       </NavLink>
     </nav>
